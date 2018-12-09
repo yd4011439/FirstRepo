1cb461acc3d2f2b332f3b40346b5f6fb2bdf4583
Yadav Dhakal <yd4011439@gmail.com>
2018-12-09 20:56:48 +0545

    Initial commit

---
+++ header.h
@@ -0,0 +1,12 @@
+#pragma once
+class animal{
+private:
+	std::string name;
+	std::string cellType;
+public:
+	inline void setName(std::string name){ this.name = name}
+	inline void setCellType(std::string cellType){ this.cellType = cellType}
+	void printDetails(){
+		std::cout<<"Name: "<<name<<" cellType: "<<cellType<<std::endl;
+	}
+};
---
+++ name.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main(){
+	
+	printf("This is a great experiene for us to be here");
+	return 0;
+}
