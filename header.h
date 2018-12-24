83c850dd24043b7fdb704da6f5ecf3ed8bf658d7
Yadav Dhakal <yd4011439@gmail.com>
2018-12-24 13:11:58 +0545

    header.h

--- header.h
+++ header.h
@@ -4,9 +4,18 @@ private:
 	std::string name;
 	std::string cellType;
 public:
-	inline void setName(std::string name){ this.name = name}
-	inline void setCellType(std::string cellType){ this.cellType = cellType}
+	void setName(std::string name){ this.name = name;}
+	void setCellType(std::string cellType){ this.cellType = cellType;}
 	void printDetails(){
-		std::cout<<"Name: "<<name<<" cellType: "<<cellType<<std::endl;
+		std::cout<<"Name: "<<name<<" cellType: 	"<<cellType<<std::endl;
 	}
+};
+
+class Dog{
+private:
+	std::string color;
+	int headRadius;
+public:
+	void setColor(std::string color){this.color = color;}
+	void setHeadRadius(int radius){headRadius = radius;}
 };
